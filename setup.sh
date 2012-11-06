bdd3b2d4f122775add7a8741f493a0072434acfb
Dmitry Koplyarov <koplyarov@kda-laptop.(none)>
2012-11-07 02:35:07 +0400

    Modified setup.sh, moved some stuff to toolkit.sh

--- setup.sh
+++ setup.sh
@@ -1,72 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
 VIM_DIR="$HOME/.vim"
 CWD=`pwd`
+SCRIPT_NAME=`basename $0`
 SCRIPT_DIR=`dirname $CWD/$0`
 
-Log() {
-	echo $@ >&2
-}
+source "$SCRIPT_DIR/toolkit.sh"
+SCRIPT_DIR=`RemoveDots $SCRIPT_DIR`
 
-Fail() {
-	Log $@
-	exit 1
-}
 
-Try() {
-	$@
-	if [ $? -ne 0 ]; then
-		Fail "$* failed!"
-	fi
-}
-
-MkDirIfAbsent() {
-	if [ ! -e $1 ]; then
-		Try mkdir $1
-	fi
-}
-
-ClearLink() {
-	SRC=`readlink $2`
-	if [ $? -eq 0 ]; then
-		if [ "$SRC" = "$1" ]; then
-			Log "Removing $2 -> $1"
-			Try rm $2
-		else
-			Log "$2 is not a link to $1, ignoring it"
-		fi
-	fi
-}
-
-RemoveIfEmpty() {
-	if [ \( -d $1 \) ]; then
-		if [ -z "`ls -1A $1`" ]; then
-			Log "$1 is empty, removing it"
-			Try rm -r $1
-		fi
-	fi
-}
-
-if [ "x$1" = "x--remove" ]; then
-	patch --no-backup --reject-file=- -f -R -p1 < clang_complete.patch
-	if [ $? -ne 0 ]; then
-		Log "WARNING: Could not patch clang_complete"
-	fi
-
-	Try ClearLink "$SCRIPT_DIR/pathogen_bundle" "$VIM_DIR/bundle"
-	Try ClearLink "$SCRIPT_DIR/pathogen/autoload/pathogen.vim" "$VIM_DIR/autoload/pathogen.vim"
-	Try ClearLink "$SCRIPT_DIR/my-snippets" "$VIM_DIR/my-snippets"
-
-	Try RemoveIfEmpty "$VIM_DIR/autoload"
-else
+if [ $# -eq 0 ]; then
 	Try MkDirIfAbsent "$VIM_DIR"
 	Try MkDirIfAbsent "$VIM_DIR/autoload"
 
-	Try ln -s "$SCRIPT_DIR/pathogen_bundle" "$VIM_DIR/bundle"
-	Try ln -s "$SCRIPT_DIR/pathogen/autoload/pathogen.vim" "$VIM_DIR/autoload/pathogen.vim"
-	Try ln -s "$SCRIPT_DIR/my-snippets" "$VIM_DIR/my-snippets"
+	Try CreateLink "$SCRIPT_DIR/pathogen_bundle" "$VIM_DIR/bundle"
+	Try CreateLink "$SCRIPT_DIR/pathogen/autoload/pathogen.vim" "$VIM_DIR/autoload/pathogen.vim"
+	Try CreateLink "$SCRIPT_DIR/my-snippets" "$VIM_DIR/my-snippets"
 
-	Try patch -p1 < clang_complete.patch
+	Try ApplyPatch clang_complete.patch
 
+	Log "$DELIM"
+	Log "vimstuff installed!"
 	Log "Add 'source $SCRIPT_DIR/vimrc' to your $HOME/.vimrc file"
+else
+	case "x$1" in
+	"x--remove"*)
+		RevertPatch clang_complete.patch
+
+		Try ClearLink "$SCRIPT_DIR/pathogen_bundle" "$VIM_DIR/bundle"
+		Try ClearLink "$SCRIPT_DIR/pathogen/autoload/pathogen.vim" "$VIM_DIR/autoload/pathogen.vim"
+		Try ClearLink "$SCRIPT_DIR/my-snippets" "$VIM_DIR/my-snippets"
+
+		Try RemoveIfEmpty "$VIM_DIR/autoload"
+		;;
+	*)
+		Fail "usage: $SCRIPT_NAME [--remove]"
+		;;
+	esac
 fi
---
+++ toolkit.sh
@@ -0,0 +1,118 @@
+DELIM='==============================================='
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
+RemoveDots() {
+	echo $* | sed 's@/\./@/@g' | sed 's@/\.$@@g'
+}
+
+Log() {
+	if [ "`basename $SHELL`" != "bash" ]; then
+		case $1 in
+		"Info"*)
+			;;
+		"Warning"*)
+			;;
+		"Error"*)
+			;;
+		*)
+			LOGLEVEL="[Info] "
+			;;
+		esac
+		echo "{vimstuff setup} $LOGLEVEL$*" >&2
+	else	
+		case $1 in
+		"Info"*)
+			LOGLEVEL="[$1]"
+			MSGCOLOR=$COL_GREEN
+			shift
+			;;
+		"Warning"*)
+			LOGLEVEL="[$1]"
+			MSGCOLOR=$COL_YELLOW
+			shift
+			;;
+		"Error"*)
+			LOGLEVEL="[$1]"
+			MSGCOLOR=$COL_RED
+			shift
+			;;
+		*)
+			LOGLEVEL="[Info]"
+			MSGCOLOR=$COL_GREEN
+			;;
+		esac
+
+		echo -e $COL_MAGENTA"{vimstuff setup}"$MSGCOLOR"$LOGLEVEL"$COL_RESET "$*" >&2
+	fi
+}
+
+Fail() {
+	Log "$DELIM"
+	Log Error $@
+	exit 1
+}
+
+Try() {
+	$@
+	if [ $? -ne 0 ]; then
+		Fail "$* failed!"
+	fi
+}
+
+MkDirIfAbsent() {
+	if [ ! -e $1 ]; then
+		Log "Creating directory $1"
+		Try mkdir $1
+	fi
+}
+
+CreateLink() {
+	Log "Creating $2 -> $1"
+	if [ -e "$2" ]; then
+		Fail "$2 exists!"
+	fi
+	Try ln -s $@
+}
+
+ClearLink() {
+	SRC=`readlink $2`
+	if [ $? -eq 0 ]; then
+		if [ "$SRC" = "$1" ]; then
+			Log "Removing $2 -> $1"
+			Try rm $2
+		else
+			Log "$2 is not a link to $1, ignoring it"
+		fi
+	fi
+}
+
+RemoveIfEmpty() {
+	if [ \( -d $1 \) ]; then
+		if [ -z "`ls -1A $1`" ]; then
+			Log "$1 is empty, removing it"
+			Try rm -r $1
+		fi
+	fi
+}
+
+ApplyPatch() {
+	Log "Applying patch $1"
+	Try patch -p1 < $1
+}
+
+RevertPatch() {
+	Log "Reverting patch $1"
+	patch --no-backup --reject-file=- -f -R -p1 < $1
+	if [ $? -ne 0 ]; then
+		Log Warning "Could not revert $1!"
+	fi
+}
+
+
