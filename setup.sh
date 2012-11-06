b8e000b45c952f048c72a5b3449155c5a654ff4d
Dmitry Koplyarov <koplyarov@spb.gs.ru>
2012-11-06 21:35:10 +0400

    Working on setup.sh

--- setup.sh
+++ setup.sh
@@ -20,11 +20,39 @@ Try() {
 	fi
 }
 
-Try ln -s $SCRIPT_DIR/pathogen_bundle $VIM_DIR/bundle
-if [ ! -e $VIM_DIR/autoload ]; then
-	Try mkdir $VIM_DIR/autoload
-fi
-Try ln -s $SCRIPT_DIR/pathogen/autoload/pathogen.vim $VIM_DIR/autoload/
-Try ln -s $SCRIPT_DIR/my-snippets $VIM_DIR/my-snippets
+ClearLink() {
+	SRC=`readlink $2`
+	if [ $? -eq 0 ]; then
+		if [ "$SRC" = "$1" ]; then
+			Log "Removing $2 -> $1"
+			Try rm $2
+		else
+			Log "$2 is not a link to $1, ignoring it"
+		fi
+	fi
+}
 
-Log "Add 'source $SCRIPT_DIR/vimrc' to your $HOME/.vimrc file"
+RemoveIfEmpty() {
+	if [ \( -d $1 \) ]; then
+		if [ -z "`ls -1A $1`" ]; then
+			Log "$1 is empty, removing it"
+			Try rm -r $1
+		fi
+	fi
+}
+
+if [ "x$1" = "x--remove" ]; then
+	Try ClearLink $SCRIPT_DIR/pathogen_bundle $VIM_DIR/bundle
+	Try ClearLink $SCRIPT_DIR/pathogen/autoload/pathogen.vim $VIM_DIR/autoload/pathogen.vim
+	Try ClearLink $SCRIPT_DIR/my-snippets $VIM_DIR/my-snippets
+	Try RemoveIfEmpty "$VIM_DIR/autoload"
+else
+	Try ln -s $SCRIPT_DIR/pathogen_bundle $VIM_DIR/bundle
+	if [ ! -e $VIM_DIR/autoload ]; then
+		Try mkdir $VIM_DIR/autoload
+	fi
+	Try ln -s $SCRIPT_DIR/pathogen/autoload/pathogen.vim $VIM_DIR/autoload/
+	Try ln -s $SCRIPT_DIR/my-snippets $VIM_DIR/my-snippets
+
+	Log "Add 'source $SCRIPT_DIR/vimrc' to your $HOME/.vimrc file"
+fi
