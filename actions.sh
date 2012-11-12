1dc4d6c08f3fa0c93be664b13f1c7c9b458d6a47
Dmitry Koplyarov <koplyarov@spb.gs.ru>
2012-11-12 22:41:50 +0400

    Fixed Patch action

--- actions.sh
+++ actions.sh
@@ -7,8 +7,8 @@ do_Symlink() { ln -s $1 $2; }
 undo_Symlink() { [ "`readlink $2`" != "$1" ] || rm $2; }
 
 msg_Patch() { echo "Applying patch $1"; }
-do_Patch() { patch -p1 < $1; }
-undo_Patch() { patch --no-backup --reject-file=- -f -R -p1 < $1; }
+do_Patch() { patch --dry-run -p1 < $1 && patch -p1 < $1; }
+undo_Patch() { patch --dry-run --no-backup --reject-file=- -f -R -p1 < $1 && patch --no-backup --reject-file=- -f -R -p1 < $1; }
 
 msg_Cp() { echo "Copying $1 to $2"; }
 do_Cp() { cp $1 $2; }
