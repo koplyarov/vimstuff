04e27c85b90db1f093308a2909234c73506f79ad
Dmitry Koplyarov <koplyarov@spb.gs.ru>
2013-10-14 17:39:25 +0400

    Calling SmartTab function as a snipMate fallback

--- setup.sh
+++ setup.sh
@@ -92,6 +92,7 @@ done
 AddAction VIMSTUFF_SETUP Patch "$VIM_DIR/bundle" -p1 fuf.patch
 AddAction VIMSTUFF_SETUP Patch "$VIM_DIR/bundle" -p1 fuf_buffertag.patch
 AddAction VIMSTUFF_SETUP Patch "$VIM_DIR/bundle" -p1 nerdcommenter.patch
+AddAction VIMSTUFF_SETUP Patch "$VIM_DIR/bundle" -p1 snipMate.patch
 
 AddAction VIMSTUFF_SETUP AddVimCfgLine "$HOME/.vimrc" "source $SCRIPT_DIR/vimrc"
 
